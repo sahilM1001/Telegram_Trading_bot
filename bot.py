fed1b96f88195595363c88bf0b6b9efbd1a2417a
sahil <sahilmansuri531@gmail.com>
2023-12-06 08:50:48 +0530

    Added error handling and support for another channel

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import re, os
 from telethon import TelegramClient, events, sync
 from common.filters import buy_filter, exit_filter, book_profits_filter
+from common.helper import get_scrip_name_from_scrip_master
 from dotenv import load_dotenv, find_dotenv
 from brokers.FivePaisa import FivePaisa
+import sys
 
 load_dotenv(find_dotenv())
 
@@ -12,7 +14,7 @@ api_hash = os.getenv('API_HASH')
 five_paisa = FivePaisa()
 
 
-with TelegramClient('session_1_dec', api_id , api_hash) as client:
+with TelegramClient('session_7_dec', api_id , api_hash) as client:
     
     client.start()
     print("Client is connected")
@@ -32,7 +34,10 @@ with TelegramClient('session_1_dec', api_id , api_hash) as client:
     # 5Paisa telegram channel id -> 1400983952
     @client.on(events.NewMessage(chats=1400983952)) 
     async def handler(event):
-        print("New message received")
+        """
+        This handler is used to get trade recommendations from 5Paisa channel
+        """
+        print("New message received on 5PAISA Channel")
         print("Event text: ", event.raw_text)
         recommendation = event.raw_text
 
@@ -41,47 +46,145 @@ with TelegramClient('session_1_dec', api_id , api_hash) as client:
         book_profits_regex = re.compile(r'\b(?:book profits)\b', re.IGNORECASE)
 
         if buy_regex.search(recommendation):
-            print("This is a buy call.")
-            
-            scrip_name_regex = r'(?<=\bBUY\b\s)(.*)'
-            price_regex = r'\bCMP\s*:\s*(.*)'
-            stop_loss_regex = r'\bSL\s*:\s*(.*)'
-            target_regex = r'\bTGT\s*:\s*(.*)'
+            try:
+                print("This is a buy call.")
+                
+                scrip_name_regex = r'(?<=\bBUY\b\s)(.*)'
+                price_regex = r'\bCMP\s*:\s*(.*)'
+                stop_loss_regex = r'\bSL\s*:\s*(.*)'
+                target_regex = r'\bTGT\s*:\s*(.*)'
 
-            transaction_details = buy_filter(recommendation, scrip_name_regex, price_regex, stop_loss_regex, target_regex)
-            print("Cleaned transaction details received: ", transaction_details)
-            
-            position_amount = five_paisa.get_position_amount()
-            share_qty = five_paisa.get_share_qty(transaction_details['price'], position_amount)
+                transaction_details = buy_filter(recommendation, scrip_name_regex, price_regex, stop_loss_regex, target_regex)
+                print("Cleaned transaction details received: ", transaction_details)
+                
+                position_amount = five_paisa.get_position_amount()
+                share_qty = five_paisa.get_share_qty(transaction_details['price'], position_amount)
 
-            if share_qty > 0:
-                five_paisa.buy(transaction_details['scrip'], transaction_details['stop_loss'], transaction_details['target'], share_qty, transaction_details['price'])
-            else:
-                print("Not taking trade because share qty is 0 or limit not available")
+                if share_qty > 0:
+                    five_paisa.buy(transaction_details['scrip'], transaction_details['stop_loss'], transaction_details['target'], share_qty, transaction_details['price'])
+                else:
+                    print("Not taking trade because share qty is 0 or limit not available")
+            except Exception as e:
+                # TODO add exception alerting to telegram channel for manual intervention
+                print("Exception occured while processing BUY call from 5 Paisa")
         elif exit_regex.search(recommendation):
-            print("This is a exit call.")
-            scrip_name_regex = r'Exit from\s*\n*\s*(.*)'
-            target_regex = r'\bAT:\s*(.*)'
+            try:
+                print("This is a exit call.")
+                scrip_name_regex = r'Exit from\s*\n*\s*(.*)'
+                target_regex = r'\bAT\s*:\s*(.*)'
 
-            transaction_details = exit_filter(recommendation, scrip_name_regex, target_regex)
-            print("Cleaned transaction details received: ", transaction_details)
+                transaction_details = exit_filter(recommendation, scrip_name_regex, target_regex)
+                print("Cleaned transaction details received: ", transaction_details)
+                
+                """print("Transaction details of target: ", type(transaction_details['Target']))
+                if transaction_details['target'] == None:
+                    target_regex = r'\bAt :\s*(.*)'
+                    transaction_details = exit_filter(recommendation, scrip_name_regex, target_regex)
 
-            five_paisa.sell(transaction_details['scrip'], transaction_details['target'])
+                print("transaction details: ", transaction_details)"""
+                five_paisa.sell(transaction_details['scrip'], transaction_details['target'])
+            except Exception as e:
+                # TODO add exception alerting to telegram channel for manual intervention
+                print("Exception occured while processing EXIT call from 5 Paisa")
             
         elif book_profits_regex.search(recommendation):
-            print("This is a call to book profits")
-            
-            scrip_name_regex = r'Book Profits in\s*\n*\s*(.*)'
-            target_regex = r'\bAT CMP :\s*(.*)'
-            
-            transaction_details = book_profits_filter(recommendation, scrip_name_regex, target_regex)
-            five_paisa.sell(transaction_details['scrip'], transaction_details['target'])
+            try:
+                print("This is a call to book profits")
+                
+                scrip_name_regex = r'Book Profits in\s*\n*\s*(.*)'
+                target_regex = r'\bAT CMP :\s*(.*)'
+
+                transaction_details = book_profits_filter(recommendation, scrip_name_regex, target_regex)
+                five_paisa.sell(transaction_details['scrip'], transaction_details['target'])
+            except Exception as e:
+                # TODO add exception alerting to telegram channel for manual intervention
+                print("Exception occured while processing BOOK PROFITS call from 5 Paisa")
             
         else:
             print("No regex match")
 
         # TODO create broker connectors to execute orders
         
+    @client.on(events.NewMessage(chats=1354170870)) 
+    async def handler_2(event):
+        """
+        This handler is used to filter trade recommendations from Angel One's Advisory channel
+        """
+        print("New message received on ANGEL ONE SECOND channel")
+        print("Event text: ", event.raw_text)
+        recommendation = event.raw_text
+
+        buy_short_term_regex = re.compile(r'\bBUY', re.IGNORECASE)
+       
+        exit_regex = re.compile(r'\b(Exit )\b', re.IGNORECASE)
+        book_profits_regex = re.compile(r'\b(Book profit In)\b', re.IGNORECASE)
+
+        if buy_short_term_regex.search(recommendation):
+            try: 
+                print("This is a buy call.")
+                
+                scrip_name_regex = r'BUY\s+(.+?)\s\d+\s+(?:shares|lot) at \d+\.'
+                price_regex = r'at (\d+)'
+                stop_loss_regex = r'Message : SL (\d+) TGT'
+                target_regex= r'TGT (\d+) MODIFY'
+
+                
+                transaction_details = buy_filter(recommendation, scrip_name_regex, price_regex, stop_loss_regex, target_regex)
+                transaction_details['scrip'] = get_scrip_name_from_scrip_master(five_paisa.scrip_master, transaction_details['scrip'], "channel_3")
+
+                print("Cleaned transaction details received: ", transaction_details)
+                print("---------------------------------------------")
+                print("---------------------------------------------")
+                
+                
+                position_amount = five_paisa.get_position_amount()
+                share_qty = five_paisa.get_share_qty(transaction_details['price'], position_amount)
+
+                if share_qty > 0:
+                    five_paisa.buy(transaction_details['scrip'], transaction_details['stop_loss'], transaction_details['target'], share_qty, transaction_details['price'])
+                else:
+                    print("Not taking trade because share qty is 0 or limit not available")
+            except Exception as e:
+                # TODO add exception alerting to telegram channel for manual intervention
+                print("Exception occured while processing BUY call from Angel One")
+        elif exit_regex.search(recommendation):
+            try:
+                print("This is a exit call.")
+                scrip_name_regex =""
+                target_regex = ""
+                if "@" in recommendation:
+                    scrip_name_regex =  r'EXIT\s+([^@]+) @'
+                    target_regex = r'@(\d+\.\d+)'
+                else:
+                    scrip_name_regex =  r'EXIT\s+(.+)'
+                    target_regex = r'@(\d+\.\d+)'
+
+                transaction_details = exit_filter(recommendation, scrip_name_regex, target_regex)
+                print("Cleaned transaction details received: ", transaction_details)
+
+                five_paisa.sell(transaction_details['scrip'], transaction_details['target'])
+            except Exception as e:
+                # TODO add exception alerting to telegram channel for manual intervention
+                print("Exception occured while processing EXIT CALL from Angel One")
+                pass
+            
+        elif book_profits_regex.search(recommendation):
+            try:
+                print("This is a call to book profits")
+                
+                scrip_name_regex = r'BOOK PROFIT IN\s+([^\s@]+) @'
+                target_regex = r'@(\d+\.\d+)'
+                
+                transaction_details = book_profits_filter(recommendation, scrip_name_regex, target_regex)
+                transaction_details['scrip'] = get_scrip_name_from_scrip_master(five_paisa.scrip_master, transaction_details['scrip'], "channel_3")
+                
+                print("cleaned transaction details in book profits: ", transaction_details)
+                five_paisa.sell(transaction_details['scrip'], transaction_details['target'])
+            except Exception as e:
+                # TODO add exception alerting to telegram channel for manual intervention
+                print("Exception occured while processing BOOK PROFITS call from Angel One")    
+        else:
+            print("No regex match")
 
 
     client.run_until_disconnected()
--- common/helper.py
+++ common/helper.py
@@ -1,5 +1,6 @@
 import re
-    
+import pandas as pd
+from datetime import date    
 def evaluate_regex_and_return_value(regex, message):
     """
     This is a common function to evaluate a given regex and return value if there is a match
@@ -19,4 +20,27 @@ def evaluate_regex_and_return_value(regex, message):
     else:
         print("No text found for the sent regex")
 
+def get_scrip_name_from_scrip_master(scrip_master, scrip_name, channel_num):
+    
+    current_year = date.today().year
+    scrip_name = scrip_name.strip()
+    print("above if condition match")
+    scrip_row = scrip_master[scrip_master['Name'] == scrip_name]
+    if scrip_row.shape[0] > 0:
+        print("Inside if condition match")
+        row = ["", ""]
+        return scrip_name
+        row[0] = scrip_master[scrip_master['Name'] == scrip_name].iloc[0]['Scripcode']
+        row[1] = scrip_master[scrip_master['Name'] == scrip_name].iloc[0]['Exch']
+        return row
+    else:
+        if channel_num == "channel_3":
+            print("Inside channel 3")
+            scrip_arr = scrip_name.split(" ")
+            search_key = scrip_arr[0] + " " + scrip_arr[1] + " " + scrip_arr[2].title() + " " + str(current_year) + " " + scrip_arr[-1] + " " + str(format(float(scrip_arr[-2]),'.2f'))
+            return search_key
+            row = ["", ""] 
+            row[0] = scrip_master[scrip_master['Name'] == search_key].iloc[0]['Scripcode']
+            row[1] = scrip_master[scrip_master['Name'] == search_key].iloc[0]['Exch']
+            return row
 
